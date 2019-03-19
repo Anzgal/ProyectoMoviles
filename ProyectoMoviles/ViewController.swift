4dc7675e76c05557fb25e87d77f4b42da13be132
alister.estrada <alister.estrada@gmail.com>
2019-03-19 02:13:12 -0600

    Initial Commit

---
+++ ProyectoMoviles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProyectoMoviles
+//
+//  Created by Alister Estrada Cueto on 3/19/19.
+//  Copyright © 2019 Alister Estrada Cueto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
