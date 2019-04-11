8393edd2ab0f6dd9707a3bce94b4ca04e0b8127b
Martín Anzures Galván <agalvan70@hotmail.com>
2019-04-11 14:26:08 -0500

    fdasdf

--- ProyectoMoviles/ModelosTableViewController.swift
+++ ProyectoMoviles/ModelosTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ModelosTableViewController.swift
+//  FotosTableViewController.swift
 //  ProyectoMoviles
 //
 //  Created by Martin Anzures on 4/11/19.
@@ -8,83 +8,94 @@
 
 import UIKit
 
-class ModelosTableViewController: UITableViewController {
-
+class ModelosTableViewController: UITableViewController  {
+    
+    var tableArray = [String] ()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+        
+        
+        print(self.tableArray)
+        
+        
+        
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
+    
+}
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
+extension ModelosTableViewController {
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
+        
+        
+        cell.textLabel?.text = self.tableArray[indexPath.row]
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return self.tableArray.count
+        
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(self.tableArray[indexPath.row])
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
-
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+     
+     let delete = UITableViewRowAction(style: .destructive, title: "Delete") { (action, indexPath) in
+     // delete item at indexPath
+     self.tableArray.remove(at: indexPath.row)
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     print(self.tableArray)
+     }
+     
+     let share = UITableViewRowAction(style: .default, title: "Share") { (action, indexPath) in
+     // share item at indexPath
+     print("I want to share: \(self.tableArray[indexPath.row])")
+     }
+     
+     share.backgroundColor = UIColor.lightGray
+     
+     return [delete, share]
+     
+     }*/
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        let siguiente = segue.destination as! VideosViewViewController
+        //let prueba = textoFila
+        //siguiente.linkRecibido = self.tableArray[IndexPath.row]
+        siguiente.linkRecibido = "http://ebookfrenzy.com/ios_book/movie/movie.mov"
+        
+        //print("El textoFila es: " + textoFila)
+        
+        /*
+         if segue.identifier == "showImage" {
+         
+         let siguiente = segue.destination as! PanonoViewController
+         let indexPath = sender as! IndexPath
+         siguiente.linkRecibido = sections[indexPath.section].items[indexPath.row]
+         }*/
     }
-    */
-
+    
 }
