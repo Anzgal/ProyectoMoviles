58787b4291bbe2dfe56e5e63a611493dfd36573c
AlisterEstrada <44268997+AlisterEstrada@users.noreply.github.com>
2019-04-11 03:47:56 -0500

    Add files via upload
    Agregué la tabla de proyectos pero tiene un error y el calendario supuestamente ya funciona

---
+++ ProyectoMoviles/ProyectoMoviles/ProyectoCreadoViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ProyectoCreadoViewController.swift
+//  ProyectoMoviles
+//
+//  Created by Alister Estrada Cueto on 3/21/19.
+//  Copyright © 2019 Alister Estrada Cueto. All rights reserved.
+//
+
+import UIKit
+
+class ProyectoCreadoViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return colors.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return colors[row]
+    }
+    
+    
+    @IBOutlet weak var pickerView: UIPickerView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    let colors = ["Alister","Martin",]
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+
+}
