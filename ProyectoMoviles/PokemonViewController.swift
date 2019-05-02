980b9e681586643b688730d4bbfbf05bacddb677
alister.estrada <alister.estrada@gmail.com>
2019-05-02 15:14:51 -0500

    llave

--- ProyectoMoviles/PokemonViewController.swift
+++ ProyectoMoviles/PokemonViewController.swift
@@ -24,7 +24,7 @@ class PokemonViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/Pokemon.scn")!
+        let scene = SCNScene(named: "art.scnassets/Llave.scn")!
         
         // Set the scene to the view
         sceneView.scene = scene
