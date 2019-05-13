0f119748b6e002927213576b3d3c4fbc66a580bf
alister.estrada <alister.estrada@gmail.com>
2019-05-13 14:57:53 -0500

    Hace llamadas

--- ProyectoMoviles/DetalleViewController.swift
+++ ProyectoMoviles/DetalleViewController.swift
@@ -48,6 +48,11 @@ class DetalleViewController: UIViewController {
     
     
     
+    @IBAction func HacerLlamda(_ sender: Any) {
+        
+        var url:NSURL = NSURL(string: "tel://015554832020")!
+        UIApplication.shared.openURL(url as URL)
+    }
     
     
      // MARK: - Navigation
